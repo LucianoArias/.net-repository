b071ff71a2aad33a24539f505569291a0eb8ba1b
LucianoArias <lucianoarias1997@gmail.com>
2022-10-14 18:21:37 -0300

    ejercicio-5

---
+++ ejercicio-5/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Ingrese la longitud del vector:");
+var longitud = int.Parse(Console.ReadLine());
+
+int[] miVector = new int[longitud];
+Random numerosAleatorios = new Random();
+
+Console.WriteLine("======================");
+Console.WriteLine("Antes de invertir: ");
+for (int i = 0; i < miVector.Length; i++)
+{
+    miVector[i] = numerosAleatorios.Next(100);
+    Console.WriteLine(miVector[i]);
+}
+
+for (int indiceDelExtremoIzquierdo = 0; indiceDelExtremoIzquierdo < miVector.Length / 2; indiceDelExtremoIzquierdo++)
+{
+    int indiceDelExtremoDerecho = miVector.Length - indiceDelExtremoIzquierdo - 1;
+    int temporal = miVector[indiceDelExtremoDerecho];
+    miVector[indiceDelExtremoDerecho] = miVector[indiceDelExtremoIzquierdo];
+    miVector[indiceDelExtremoIzquierdo] = temporal;
+}
+
+Console.WriteLine("======================");
+Console.WriteLine("Después de invertir: ");
+for (int i = 0; i < miVector.Length; i++)
+{
+    Console.WriteLine(miVector[i]);
+}
