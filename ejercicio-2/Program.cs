1707a9ecb1869962c3f49dec60e48314966d0893
LucianoArias <lucianoarias1997@gmail.com>
2022-10-14 18:11:11 -0300

    ejercicio-2

---
+++ ejercicio-2/Program.cs
@@ -0,0 +1,25 @@
+﻿int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 21);
+int numeroElegido;
+int intentos = 1;
+Console.WriteLine("ADIVINA EL NÚMERO!!");
+
+do
+{
+    Console.Write("Ingrese un número del 1 al 20 para adivinar: ");
+    numeroElegido = int.Parse(Console.ReadLine());
+    if (numeroElegido > numeroSecreto)
+    {
+        Console.WriteLine("El número elegido es muy grande, vuelve a intentarlo!");
+    }
+    if (numeroElegido < numeroSecreto)
+    {
+        Console.WriteLine("El número elegido es muy chico, vuelve a intentarlo!");
+    }
+    if (numeroElegido == numeroSecreto)
+    {
+        Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: {numeroSecreto}");
+        Console.WriteLine($"Lo has logrado en {intentos} intentos!!");
+    }
+    intentos++;
+}
+while (numeroElegido != numeroSecreto);
