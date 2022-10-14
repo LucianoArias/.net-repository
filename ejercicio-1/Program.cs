838f7c2384c5a8f6a2c2b523c66f91c24d7ba18e
LucianoArias <lucianoarias1997@gmail.com>
2022-10-14 18:07:56 -0300

    ejercicio-1

---
+++ ejercicio-1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.WriteLine("Ingrese su nombre:");
+string name = Console.ReadLine();
+Console.WriteLine($"¡Hola {name}!");
+
+Console.WriteLine("Desea continuar (S/N): ");
+string text = Console.ReadLine();
+
+if (text.ToUpper() == "S")
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese su nombre:");
+    name = Console.ReadLine();
+    Console.WriteLine($"¡Hola {name}!");
+}
+else if (text.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+}
+else
+{
+    Console.WriteLine("Opción no válida");
+}
