75cf07ffc45336fb55e2b382d66d246e7661d200
LucianoArias <lucianoarias1997@gmail.com>
2022-10-19 14:29:52 -0300

    ejercicio-7

---
+++ ejercicio-7/Modelo/Billetera.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_7.Modelo
+{
+    internal class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+        public decimal Total()
+        {
+            int totalDe10 = this.BilletesDe10 * 10;
+            int totalDe20 = this.BilletesDe20 * 20;
+            int totalDe50 = this.BilletesDe50 * 50;
+            int totalDe100 = this.BilletesDe100 * 100;
+            int totalDe200 = this.BilletesDe200 * 200;
+            int totalDe500 = this.BilletesDe500 * 500;
+            int totalDe1000 = this.BilletesDe1000 * 1000;
+            return totalDe10 + totalDe20 + totalDe50 + totalDe100 + totalDe200 + totalDe500 + totalDe1000;
+        }
+
+        public Billetera Combinar(Billetera segundaBilletera)
+        {
+            Billetera nuevaBilletera = new()
+            {
+                BilletesDe10 = this.BilletesDe10 + segundaBilletera.BilletesDe10,
+                BilletesDe20 = this.BilletesDe20 + segundaBilletera.BilletesDe20,
+                BilletesDe50 = this.BilletesDe50 + segundaBilletera.BilletesDe50,
+                BilletesDe100 = this.BilletesDe100 + segundaBilletera.BilletesDe100,
+                BilletesDe200 = this.BilletesDe200 + segundaBilletera.BilletesDe200,
+                BilletesDe500 = this.BilletesDe500 + segundaBilletera.BilletesDe500,
+                BilletesDe1000 = this.BilletesDe1000 + segundaBilletera.BilletesDe1000,
+            };
+            return nuevaBilletera;
+        }
+    }
+}
---
+++ ejercicio-7/Program.cs
@@ -0,0 +1,54 @@
+﻿using ejercicio_7.Modelo;
+
+Billetera[] billeteras = new Billetera[2];
+
+billeteras[0] = new Billetera();
+billeteras[1] = new Billetera();
+int contador = 0;
+
+Console.WriteLine("Ingrese la cantidad de billetes de cada billetera");
+
+foreach (var item in billeteras)
+{
+    contador++;
+    Console.WriteLine($"Billetera {contador}");
+
+    Console.WriteLine("Billetes de 10: ");
+    int b10 = int.Parse(Console.ReadLine());
+    item.BilletesDe10 = b10;
+
+    Console.WriteLine("Billetes de 20: ");
+    int b20 = int.Parse(Console.ReadLine());
+    item.BilletesDe20 = b20;
+
+    Console.WriteLine("Billetes de 50: ");
+    int b50 = int.Parse(Console.ReadLine());
+    item.BilletesDe50 = b50;
+
+    Console.WriteLine("Billetes de 100: ");
+    int b100 = int.Parse(Console.ReadLine());
+    item.BilletesDe100 = b100;
+
+    Console.WriteLine("Billetes de 200: ");
+    int b200 = int.Parse(Console.ReadLine());
+    item.BilletesDe200 = b200;
+
+    Console.WriteLine("Billetes de 500: ");
+    int b500 = int.Parse(Console.ReadLine());
+    item.BilletesDe500 = b500;
+
+    Console.WriteLine("Billetes de 1000: ");
+    int b1000 = int.Parse(Console.ReadLine());
+    item.BilletesDe1000 = b1000;
+
+    Console.WriteLine("------------------------------------------------");
+}
+
+Billetera primerBilletera = billeteras[0];
+Console.WriteLine($"Dinero total de la primer billetera: ${primerBilletera.Total()}");
+
+Billetera segundaBilletera = billeteras[1];
+Console.WriteLine($"Dinero total de la segunda billetera: ${segundaBilletera.Total()}");
+
+Billetera nuevaBilletera = billeteras[0].Combinar(billeteras[1]);
+Console.WriteLine($"Suma total de ambas billeteras ${nuevaBilletera.Total()}");
